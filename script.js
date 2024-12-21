a92ffe26d3072fa47eda68e480fc3859a15e2ef1
vaishnavi-shelke464 <vaishnavishelke464@gmail.com>
2024-12-21 22:29:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Contact Form Submission (Dummy JS for now)
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
